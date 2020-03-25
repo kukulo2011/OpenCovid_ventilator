df5ca1927ddd51d1277fa375d3ad9bedf4c7ceaa
Bill Foote <billf@jovial.com>
2020-03-24 23:09:53 -0700

    Remove stray comment

--- display_app/breezy/lib/serial.dart
+++ display_app/breezy/lib/serial.dart
@@ -87,8 +87,6 @@ class _SerialTestPageState extends State<SerialTestPage> {
     // than having to individually change instances of widgets.
     return Scaffold(
         appBar: AppBar(
-            // Here we take the value from the MyHomePage object that was created by
-            // the App.build method, and use it to set our appbar title.
             title: Text('Breezy: Test Serial'),
             actions: <Widget>[
               IconButton(
