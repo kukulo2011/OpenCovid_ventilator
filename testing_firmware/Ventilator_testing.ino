e54d9d2a02badb69b660644ea2399fb2c364389c
kukulo2011 <kukulo2011@users.noreply.github.com>
2020-04-04 21:01:24 +0200

    Added CRC + updated testing of charts via serial
    Added CRC + updated testing of charts via serial

--- testing_firmware/Ventilator_testing.ino
+++ testing_firmware/Ventilator_testing.ino
@@ -2,21 +2,27 @@
 Testing sketch for Breezy app for Arduino Nano
 */
 
+#include "crc16.h"
 
-float volume = 450;
-float flow = 120;
-float pressure = 50;
-float ppeak = 50;
-float peep = 65;
-float pmean= 35;
-float RR=85;
-float Ti=25;
-float Ttot=20;
-float O2conc=35;
-float VTo=1254.21;
-float VTi=1245;
-float MVo=1100;
-float MV2=1005;
+  float p_act=35; // actual pressure (cmH2O)
+  float slm=65; // flow (l/min)
+  float slm_sum=250; // volume (ml)
+  float p_peak=50; // peak pressure (cmH2O)
+  float p_mean=35; // mean pressure (cmH2O)
+  float peep=65; // positive end-expiratory pressure
+  float rr=85; // respiratory rate
+  float o2_perc=35; // O2 concentration
+  float ti=25; // inspiration time (s)
+  float te=20; // expiration time (s) // not printed in message
+  float i_e=0.7; // inspiraton : exspiration
+  float mvi=12; // mean volume inspiration (l/min)
+  float mve=11.8; // mean volume expiration (l/min)
+  float vti=468; // volume tidal inspiration (ml)
+  float vte=457; // volume tidal expiration (ml)
+
+  float p_o2; // O2 supply pressure
+
+  CRC16 Crc16;
  
 void setup() {
   Serial.begin(115200);
@@ -24,39 +30,73 @@ void setup() {
 
 }
 void loop() {
-  Serial.print("breezy,1,");
-  Serial.print(millis(), 1);
-  Serial.print(","); 
-  Serial.print(volume, 1);
-  Serial.print(","); 
-  Serial.print(flow, 1);
-  Serial.print(","); 
-  Serial.print(pressure, 1);
-  Serial.print(","); 
-  Serial.print(ppeak, 1);
-  Serial.print(","); 
-  Serial.print(peep, 1);
-  Serial.print(","); 
-  Serial.print(pmean, 1);
-  Serial.print(","); 
-  Serial.print(RR, 1);
-  Serial.print(","); 
-  Serial.print(Ti, 1);
-  Serial.print(","); 
-  Serial.print(Ttot, 1);
-  Serial.print(","); 
-  Serial.print(O2conc, 1);
-  Serial.print(","); 
-  Serial.print(VTo, 1);
-  Serial.print(","); 
-  Serial.print(VTi, 1);
-  Serial.print(","); 
-  Serial.print(MVo, 1);
-  Serial.print(",");
-  Serial.print(MV2, 1);
-  Serial.print(",");
-  Serial.print("-1"); 
-  Serial.println();
+ 
+ uint16_t time = (uint16_t)millis();
+
+    char msg[200];
+  sprintf(msg, "breezy,1,%5u,", time );
+  
+  dtostrf(p_act, 5, 2, &msg[strlen(msg)]);
+  strcat(msg, ",");
+  
+  p_act=p_act+15*sin(random(0,6.28));
+
+  if (p_act>=50) p_act=25;
+  if (p_act<=0) p_act=25;
+  
+  dtostrf(slm, 5, 2, &msg[strlen(msg)]);
+  strcat(msg, ",");
+  
+  slm=slm+1;
+  
+  if (slm==101) slm=-100;
+  
+  dtostrf(slm_sum, 5, 2, &msg[strlen(msg)]);
+  strcat(msg, ",");
+  
+   slm_sum=slm_sum+10*cos(random(0,6.28));
+  
+  if (slm_sum>=800) slm_sum=230;
+  if (slm_sum<=0) slm_sum=250;
+
+  dtostrf(p_peak, 5, 1, &msg[strlen(msg)]);
+  strcat(msg, ",");
+  
+  dtostrf(p_mean, 2, 0, &msg[strlen(msg)]);
+  strcat(msg, ",");
+  
+  dtostrf(peep, 2, 0, &msg[strlen(msg)]);
+  strcat(msg, ",");
+  
+  dtostrf(rr, 2, 0, &msg[strlen(msg)]);
+  strcat(msg, ",");
+  
+  dtostrf(o2_perc, 3, 0, &msg[strlen(msg)]);
+  strcat(msg, ",");
+  
+  dtostrf(ti, 5, 2, &msg[strlen(msg)]);
+  strcat(msg, ",");
+  
+  dtostrf(i_e, 4, 1, &msg[strlen(msg)]);
+  strcat(msg, ",");
+
+  dtostrf(mvi, 4, 1, &msg[strlen(msg)]);
+  strcat(msg, ",");
+
+  dtostrf(mve, 4, 1, &msg[strlen(msg)]);
+  strcat(msg, ",");
+
+  dtostrf(vti, 3, 0, &msg[strlen(msg)]);
+  strcat(msg, ",");
+
+  dtostrf(vte, 3, 0, &msg[strlen(msg)]);
+  strcat(msg, ",");
+
+  uint16_t crc = Crc16.get_crc16(msg);
+
+  sprintf(&msg[strlen(msg)], "%5u\r\n", crc);
+  Serial.print(msg);
+  
   delay(50);
     
 }
